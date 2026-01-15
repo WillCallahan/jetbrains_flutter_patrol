0f906ca015f4ff0b6ed2f4181ab58ec369c0a880
William Callahan <wcallahan@908devices.com>
2026-01-14 22:19:00 -0500

    Scale Patrol run configuration icon

--- src/main/java/com/patrol/jetbrains/run/PatrolRunConfigurationType.java
+++ src/main/java/com/patrol/jetbrains/run/PatrolRunConfigurationType.java
@@ -2,6 +2,7 @@ package com.patrol.jetbrains.run;
 
 import com.intellij.execution.configurations.ConfigurationTypeBase;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.util.IconUtil;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.Icon;
@@ -9,9 +10,10 @@ import javax.swing.Icon;
 public final class PatrolRunConfigurationType extends ConfigurationTypeBase {
   public static final String ID = "PATROL_RUN_CONFIGURATION";
   private static final Icon PATROL_ICON = IconLoader.getIcon("/icons/patrol.svg", PatrolRunConfigurationType.class);
+  private static final Icon SMALL_PATROL_ICON = IconUtil.scale(PATROL_ICON, null, 0.8f);
 
   public PatrolRunConfigurationType() {
-    super(ID, "Patrol Test", "Run Patrol Flutter tests", PATROL_ICON);
+    super(ID, "Patrol Test", "Run Patrol Flutter tests", SMALL_PATROL_ICON);
     addFactory(new PatrolRunConfigurationFactory(this, "Patrol Test", PatrolCommandMode.TEST));
     addFactory(new PatrolRunConfigurationFactory(this, "Patrol Develop", PatrolCommandMode.DEVELOP));
   }
